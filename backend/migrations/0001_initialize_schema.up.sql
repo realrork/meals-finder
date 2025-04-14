113b3f6268e6a19f1e256b1d0b5967c13e6b39a0
Milosz Borowiecki <96954563+miloszbo@users.noreply.github.com>
2025-04-15 01:42:16 +0200

    Add more NOT NULL

--- backend/migrations/0001_initialize_schema.up.sql
+++ backend/migrations/0001_initialize_schema.up.sql
@@ -2,14 +2,14 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(40) UNIQUE,
-    created_at TIMESTAMP,
-    passwd VARCHAR(40),
-    email VARCHAR(50),
+    created_at TIMESTAMP NOT NULL,
+    passwd VARCHAR(40) NOT NULL,
+    email VARCHAR(50) NOT NULL,
     name VARCHAR(40),
     surname VARCHAR(40),
-    phone VARCHAR(12),
-    age INTEGER,
-    sex VARCHAR(13),
+    phone VARCHAR(12) NOT NULL,
+    age INTEGER NOT NULL,
+    sex VARCHAR(13) NOT NULL,
     weight INTEGER,
     height INTEGER,
     BMI INTEGER
@@ -18,18 +18,18 @@ CREATE TABLE users (
 -- Table: recipes
 CREATE TABLE recipes (
     id int SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    synopsis VARCHAR(100),
-    time INTEGER, -- Preparation time in minutes
-    difficulty INTEGER
+    name VARCHAR(100) NOT NULL,
+    synopsis VARCHAR(100) NOT NULL,
+    time INTEGER NOT NULL, -- Preparation time in minutes
+    difficulty INTEGER NOT NULL
 );
 
 -- Table: reviews
 CREATE TABLE reviews (
     id SERIAL PRIMARY KEY,
-    recipe_id int,
-    user_id INTEGER,
-    review INTEGER,
+    recipe_id int NOT NULL,
+    user_id INTEGER NOT NULL,
+    review INTEGER NOT NULL,
     FOREIGN KEY (recipe_id) REFERENCES recipes(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
@@ -37,14 +37,14 @@ CREATE TABLE reviews (
 -- Table: ingredients
 CREATE TABLE ingredients (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(30) NOT NULL
 );
 
 -- Table: recipes_ingredients
 CREATE TABLE recipes_ingredients (
-    recipe_id int,
-    ingredient_id INTEGER,
-    amount int,
+    recipe_id int NOT NULL,
+    ingredient_id INTEGER NOT NULL,
+    amount int NOT NULL,
     unit INTEGER DEFAULT 0,
     FOREIGN KEY (recipe_id) REFERENCES recipes(id),
     FOREIGN KEY (ingredient_id) REFERENCES ingredients(id)
@@ -53,21 +53,21 @@ CREATE TABLE recipes_ingredients (
 -- Table: tags_types
 CREATE TABLE tags_types (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(30)
+    name VARCHAR(30) NOT NULL
 );
 
 -- Table: tags
 CREATE TABLE tags (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(30),
-    type_id INTEGER,
+    name VARCHAR(30) NOT NULL,
+    type_id INTEGER NOT NULL,
     FOREIGN KEY (type_id) REFERENCES tags_types(id)
 );
 
 -- Table: recipes_tags
 CREATE TABLE recipes_tags (
-    recipe_id int,
-    tag_id INTEGER,
+    recipe_id int NOT NULL,
+    tag_id INTEGER NOT NULL,
     FOREIGN KEY (recipe_id) REFERENCES recipes(id),
     FOREIGN KEY (tag_id) REFERENCES tags(id)
 );
