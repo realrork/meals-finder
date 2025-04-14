a836bf78a8b6912ec8e2ce88b10c73fad4445ff4
Milosz Borowiecki <96954563+miloszbo@users.noreply.github.com>
2025-04-15 00:57:17 +0200

    Add down migration

---
+++ backend/migrations/0001_initalize_schema.down.sql
@@ -0,0 +1,15 @@
+DROP TABLE users;
+
+DROP TABLE recipes;
+
+DROP TABLE reviews;
+
+DROP TABLE ingredients;
+
+DROP TABLE recipes_ingredients;
+
+DROP TABLE tags_types;
+
+DROP TABLE tags;
+
+DROP TABLE recipes_tags;
